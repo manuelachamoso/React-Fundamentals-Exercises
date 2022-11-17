8c9d0915a6bb4311985fe6f4df513d5143506ba1
Manuela Chamoso <manuela.chamoso@gmail.com>
2022-11-17 18:38:00 +0100

    create a button to check how many likes a photo receive

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,12 +1,19 @@
 import corgi from "./corgi.jpg";
 import "./App.css";
+import {useState} from 'react'
 
 const App = () => {
+  const [like, setLike] = useState(0)
+
+  const increase = () => {
+    setLike(like + 1)
+  }
   return (
     <div className="container">
       <h2>Like this photo!</h2>
       <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
+      <button style={{background: 'red', color: 'white', cursor:'pointer'}} onClick={increase}>Like</button>
+      <p>Amount of likes: {like}</p>
     </div>
   );
 };
